bcaf432c2fd17f72a4539aedcec48b1ac6bcc316
Bill Flores <bilizen3@gmail.com>
2019-04-13 01:16:43 -0500

    add crashlytics explicit and implicit :recycle:

--- app/src/main/java/com/flores/products_firebase_android/CrashlyticsActivity.kt
+++ app/src/main/java/com/flores/products_firebase_android/CrashlyticsActivity.kt
@@ -2,6 +2,8 @@ package com.flores.products_firebase_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.crashlytics.android.Crashlytics
+import kotlinx.android.synthetic.main.activity_crashlytics.*
 
 class CrashlyticsActivity : AppCompatActivity() {
 
@@ -9,7 +11,13 @@ class CrashlyticsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_crashlytics)
 
+        btnCrashImplicit.setOnClickListener {
+            etImplicit.text as Int
+        }
 
+        btnCrashExplicit.setOnClickListener {
+            Crashlytics.getInstance().crash()
+        }
 
     }
 }
