88c036dbb34926f36fe077d88728a7025df0fe8d
Bill Flores <billflores@Bills-iMac.local>
2019-04-15 16:45:18 -0500

    add activity Authorization

---
+++ app/src/main/java/com/flores/products_firebase_android/AuthenticationActivity.kt
@@ -0,0 +1,12 @@
+package com.flores.products_firebase_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AuthenticationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authentication)
+    }
+}
